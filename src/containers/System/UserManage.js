ed7cc93dc624db0a8964b6cc50b7366a145c5ee8
LAPTOP-G9CBJI3P\tantrungse <tantrungse@gmail.com>
2022-11-07 22:22:30 +0700

    Thiết kế Modal Thêm Mới Người Dùng Với Reactstrap

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,13 +3,15 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
 
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
@@ -25,13 +27,34 @@ class UserManage extends Component {
         }
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
 
     render() {
         console.log('check render ', this.state);
         let arrUsers = this.state.arrUsers;
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    test={'abc'}
+                />
                 <div className="title">Manage users with Eric</div>
+                <div className="mx-1">
+                    <button 
+                    className="btn btn-primary px-3"
+                    onClick={()=>this.handleAddNewUser()}><i className="fas fa-plus"></i>Add new user</button>
+                </div>
                 <div className="users-table mt-3 mx-1">
                     <table id="customers">
                         <tr>
@@ -48,7 +71,7 @@ class UserManage extends Component {
                                 <tr>
                                     <td>{item.email}</td>
                                     <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>    
+                                    <td>{item.lastName}</td>
                                     <td>{item.address}</td>
                                     <td>
                                         <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
